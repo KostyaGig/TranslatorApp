26b41229585e8777b18685260ae17525b6bcefaa
kostyagig <k.gig@list.ru>
2021-10-02 00:12:15 +0300

    initial commit

--- app/src/main/java/com/zinoview/translatorapp/MainActivity.kt
+++ app/src/main/java/com/zinoview/translatorapp/MainActivity.kt
@@ -2,12 +2,14 @@ package com.zinoview.translatorapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("tag","message")
     }
 
 }
